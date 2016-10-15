e5f68850901c5ffee8faf3384709cf043a3091e6
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-15 20:27:47 +0800

    初始化项目

---
+++ Miss-Scarlett/Miss-Scarlett/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Miss-Scarlett
+//
+//  Created by mortal on 16/10/15.
+//  Copyright © 2016年 mortal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
