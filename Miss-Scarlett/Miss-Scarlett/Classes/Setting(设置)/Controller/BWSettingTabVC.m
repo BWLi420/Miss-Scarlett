ba9eaf91e1a08b471661faac0c09b8413b5c7dd3
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-16 19:56:36 +0800

    自定义返回按钮

--- Miss-Scarlett/Miss-Scarlett/Classes/Setting(设置)/Controller/BWSettingTabVC.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Setting(设置)/Controller/BWSettingTabVC.m
@@ -17,9 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor magentaColor];
+    self.view.backgroundColor = [UIColor cyanColor];
+    //自定义导航控制器的返回按钮
+    [self customBackBtn];
 }
 
+//自定义导航控制器的返回按钮
+- (void)customBackBtn {
+    
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [backBtn setImage:[UIImage imageNamed:@"navigationButtonReturn"] forState:UIControlStateNormal];
+    [backBtn setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
+    [backBtn setTitle:@"返回" forState:UIControlStateNormal];
+    [backBtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [backBtn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    [backBtn sizeToFit];
+    [backBtn addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    
+    UIView *view = [[UIView alloc] initWithFrame:backBtn.bounds];
+    [view addSubview:backBtn];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:view];
+    
+}
+
+//点击返回按钮返回上一个界面
+- (void)backBtnClick {
+    [self.navigationController popViewControllerAnimated:YES];
+}
 
 #pragma mark - Table view data source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
