edcd06e65e979b7980e5f70a2ed76cfa20f686af
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-16 13:03:46 +0800

    解决图片渲染，tabBarButton 的点击，更改字体大小

--- Miss-Scarlett/Miss-Scarlett/Classes/Main/Controller/BWTabBarController.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Main/Controller/BWTabBarController.m
@@ -20,12 +20,55 @@
 
 @implementation BWTabBarController
 
++ (void)load{
+    //程序启动类加载到内存的时候只调用一次
+    
+    /* appearance 的简单说明
+    
+     1.必须遵守 UIAppearance 协议的类才可以使用该方法
+     2.只有带有 UI_APPEARANCE_SELECTOR 宏的属性或者方法，才可以通过 appearance 进行调用设置
+     3.使用 appearance 设置属性，必须在显示之前设置
+     */
+    //获取 UITabBarItem 的外观
+    UITabBarItem *item = [UITabBarItem appearance];
+    //设置系统 tabBarButton 的字体大小
+    //只能通过正常状态去设置
+    [item setTitleTextAttributes:@{
+                                   NSFontAttributeName:[UIFont systemFontOfSize:14]
+                                   } forState:UIControlStateNormal];
+}
+
+//+ (void)initialize {
+//    //第一次使用类或它的子类的时候调用，至少调用一次
+//}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.tabBarItem.image = [UIImage imageNamed:@"tabbar-light"];
+    //创建发布按钮，实现按钮的高亮状态
+    [self addPublishBtn];
+    
+    //更改 tabBar 的渲染颜色
+    self.tabBar.tintColor = [UIColor blackColor];
+
     //添加子控制器
     [self addChildVC];
+    
+}
+
+//创建发布按钮，实现按钮的高亮状态
+- (void)addPublishBtn {
+    //只执行一次
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        UIButton *publishBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [publishBtn setImage:[UIImage imageNamed:@"tabBar_publish_icon"] forState:UIControlStateNormal];
+        [publishBtn setImage:[UIImage imageNamed:@"tabBar_publish_click_icon"] forState:UIControlStateHighlighted];
+        [publishBtn sizeToFit];
+        publishBtn.center = CGPointMake(self.tabBar.frame.size.width * 0.5, self.tabBar.frame.size.height * 0.5);
+        [self.tabBar addSubview:publishBtn];
+    });
 }
 
 //添加子控制器
--- Miss-Scarlett/Miss-Scarlett/Classes/Publish(发布)/Controller/BWPublishVC.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Publish(发布)/Controller/BWPublishVC.m
@@ -17,8 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.tabBarItem.image = [UIImage imageWithOriginalName:@"tabBar_publish_icon"];
-    self.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabBar_publish_click_icon"];
+    //使用以下方式，由于 tabBarButton 没有高亮状态，因此在点击之后一直处于选中状态
+    //若要实现高亮状态，则创建一个 UIButton 添加到 tarBar 上,同时禁止 tabBarItem 的交互
+    self.tabBarItem.enabled = NO;
+//    self.tabBarItem.image = [UIImage imageWithOriginalName:@"tabBar_publish_icon"];
+//    self.tabBarItem.selectedImage = [UIImage imageWithOriginalName:@"tabBar_publish_click_icon"];
+//    //设置图片内边距调整位置
+//    self.tabBarItem.imageInsets = UIEdgeInsetsMake(7, 0, -7, 0);
 }
 
 @end
