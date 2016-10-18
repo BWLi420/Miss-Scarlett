eb20ba7d2d0ffe388cd4e462420325de7b3b7bb8
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-18 14:08:00 +0800

    实现非根控制器的全屏滑动返回

--- Miss-Scarlett/Miss-Scarlett/Classes/Main/Controller/BWNavController.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Main/Controller/BWNavController.m
@@ -10,7 +10,7 @@
 #import "BWNavigationBar.h"
 #import "BWNavLeftBackView.h"
 
-@interface BWNavController ()
+@interface BWNavController () <UIGestureRecognizerDelegate>
 
 @end
 
@@ -49,17 +49,65 @@
     [navBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
 
-//使用自定义的导航条
+#pragma mark - 全屏滑动返回
 - (void)viewDidLoad {
-    
+    /*
+     UITabBarIte：设置UITabBar上按钮内容
+     UINavigationItem：设置导航条哪边显示内容
+     UIBarButtonItem：决定导航条上按钮具体内容
+     */
+    //使用自定义的导航条
     BWNavigationBar *navBar = [[BWNavigationBar alloc] init];
     navBar.frame = self.navigationBar.bounds;
     [self setValue:navBar forKey:@"navigationBar"];
+    
+    /** 添加手势，实现全屏滑动返回 */
+    //禁用系统的手势（默认只有最左边才能滑动）
+    self.interactivePopGestureRecognizer.enabled = NO;
+    //创建自己的 pan 手势，使用系统自带手势的方法进行监听
+    id target = self.interactivePopGestureRecognizer.delegate;
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:target action:@selector(handleNavigationTransition:)];
+    pan.delegate = self;
+    //将手势添加到控制器的 view 上
+    [self.view addGestureRecognizer:pan];
+    
+    /* 分析：
+     滑动返回功能：导航控制器iOS7后自带
+     
+     恢复滑动返回功能：
+     
+     分析：覆盖掉系统返回按钮，滑动返回功能就失效
+     
+     需求：既要覆盖系统返回按钮，也要有滑动返回功能.
+     
+     为什么没有，谁导致滑动返回功能没有的。当覆盖掉系统返回按钮，系统额外做了一些事情，导致
+     滑动返回功能失效。
+     
+     系统额外做了什么事情，导致滑动返回功能失效？
+     
+     滑动返回功能，肯定需要手势? 猜测interactivePopGestureRecognizer，就有滑动返回功能
+     
+     验证滑动返回功能 跟interactivePopGestureRecognizer有关系
+     
+     让interactivePopGestureRecognizer手势失效，通过代理
+     
+     当覆盖掉系统返回按钮，系统额外做了一些事情，导致滑动返回功能失效
+     系统额外做了一些事情 => interactivePopGestureRecognizer的代理做了事情
+     
+     interactivePopGestureRecognizer的代理 就是 让滑动返回失效
+     */
 }
 
+//根据是否为根控制器，设置是否触发手势
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    //非根控制器触发手势
+    return self.childViewControllers.count > 1;
+}
+
+#pragma mark - 自定义 push 方法
 //自定义 push 方法，统一设置导航条的返回按钮
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
-    //方式一：super 在前面
+    //方式一：super 在前面，但是不能统一隐藏底部隐藏条（必须在 push 之前）
     /**
     [super pushViewController:viewController animated:animated];
     
@@ -78,8 +126,11 @@
         BWNavLeftBackView *backView = [BWNavLeftBackView backViewWithTitle:@"返回" image:[UIImage imageNamed:@"navigationButtonReturn"] highImage:[UIImage imageNamed:@"navigationButtonReturnClick"] target: self action:@selector(backBtnClick)];
         
         viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backView];
+        //隐藏底部导航条
+        viewController.hidesBottomBarWhenPushed = YES;
     }
     
+    //此时才会真正将子控制器添加到栈中
     [super pushViewController:viewController animated:animated];
 }
 
--- Miss-Scarlett/Miss-Scarlett/Classes/Setting(设置)/Controller/BWSettingTabVC.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Setting(设置)/Controller/BWSettingTabVC.m
@@ -7,7 +7,6 @@
 //
 
 #import "BWSettingTabVC.h"
-#import "BWNavLeftBackView.h"
 
 @interface BWSettingTabVC ()
 
@@ -19,22 +18,7 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor cyanColor];
-    //自定义导航控制器的返回按钮
-//    [self customBackBtn];
 }
-
-//自定义导航控制器的返回按钮
-//- (void)customBackBtn {
-//    
-//    BWNavLeftBackView *backView = [BWNavLeftBackView backViewWithTitle:@"返回" image:[UIImage imageNamed:@"navigationButtonReturn"] highImage:[UIImage imageNamed:@"navigationButtonReturnClick"] target: self action:@selector(backBtnClick)];
-//    
-//    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backView];
-//}
-
-////点击返回按钮返回上一个界面
-//- (void)backBtnClick {
-//    [self.navigationController popViewControllerAnimated:YES];
-//}
 
 #pragma mark - Table view data source
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
