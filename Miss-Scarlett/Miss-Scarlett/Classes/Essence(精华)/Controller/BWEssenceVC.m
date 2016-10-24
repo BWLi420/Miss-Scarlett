0f46a68a1cd4a3c508254462de1a7b7b28c73998
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-24 19:41:00 +0800

    优化首页业务逻辑

--- Miss-Scarlett/Miss-Scarlett/Classes/Essence(精华)/Controller/BWEssenceVC.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Essence(精华)/Controller/BWEssenceVC.m
@@ -16,12 +16,14 @@
 
 static NSString *const ID = @"collectionCell";
 
-#define BWColor(r,g,b) [UIColor colorWithRed:(r) / 255.0 green:(g) / 255.0 blue:(b) / 255.0 alpha:1]
-
-@interface BWEssenceVC () <UICollectionViewDataSource>
+@interface BWEssenceVC () <UICollectionViewDataSource, UICollectionViewDelegate>
 @property (nonatomic, weak) UIScrollView *topScrollView;
 @property (nonatomic, weak) UICollectionView *bottomCollectionView;
 @property (nonatomic, weak) UIButton *selectedBtn;
+/** 保存按钮的数组 */
+@property (strong, nonatomic) NSMutableArray *btnArray;
+/** 下划线 */
+@property (nonatomic, weak) UIView *underLine;
 @end
 
 @implementation BWEssenceVC
@@ -76,6 +78,7 @@ static NSString *const ID = @"collectionCell";
     //创建 collectionView，指定流水布局
     UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
     collectionView.dataSource = self;
+    collectionView.delegate = self;
     
     //注册 cell
     [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:ID];
@@ -131,9 +134,33 @@ static NSString *const ID = @"collectionCell";
         [btn setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
         [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
         
+        btn.tag = i;
+        
         CGFloat width = screenW / count;
         btn.frame = CGRectMake(i * width, 0, width, self.topScrollView.bw_height);
         [self.topScrollView addSubview:btn];
+        
+        //将按钮添加到数组中
+        [self.btnArray addObject:btn];
+        
+        //默认选中第 0 个按钮
+        if (i == 0) {
+            [self btnClick:btn];
+            
+            //给按钮添加下划线
+            UIView *underLine = [[UIView alloc] init];
+            underLine.backgroundColor = [UIColor redColor];
+            
+            [btn.titleLabel sizeToFit];
+            
+            underLine.bw_width = btn.titleLabel.bw_width;
+            underLine.bw_height = 2;
+            underLine.bw_centerX = btn.bw_centerX;
+            underLine.bw_y = btn.bw_height - underLine.bw_height;
+            
+            self.underLine = underLine;
+            [self.topScrollView addSubview:underLine];
+        }
     }
 }
 
@@ -141,6 +168,23 @@ static NSString *const ID = @"collectionCell";
     self.selectedBtn.selected = NO;
     btn.selected = YES;
     self.selectedBtn = btn;
+    
+    //移动要显示的 view
+    NSInteger i = btn.tag;
+    self.bottomCollectionView.contentOffset = CGPointMake(i * screenW, 0);
+    
+    //移动下划线的位置
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.underLine.bw_centerX = btn.bw_centerX;
+    }];
+}
+
+- (NSMutableArray *)btnArray {
+    if (_btnArray == nil) {
+        _btnArray = [NSMutableArray array];
+    }
+    return _btnArray;
 }
 
 #pragma mark - UICollectionViewDataSource
@@ -152,8 +196,24 @@ static NSString *const ID = @"collectionCell";
     
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:ID forIndexPath:indexPath];
     
-    cell.backgroundColor = BWColor(arc4random_uniform(256), arc4random_uniform(256), arc4random_uniform(256));
-    
+    //移除之前控制器的 view
+    [cell.contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    //切换子控制器的 view
+    UITableViewController *tabVC = self.childViewControllers[indexPath.row];
+    tabVC.view.frame = [UIScreen mainScreen].bounds;
+    tabVC.tableView.contentInset = UIEdgeInsetsMake(64 + self.topScrollView.bw_height, 0, 49, 0);
+    [cell.contentView addSubview:tabVC.view];
+
     return cell;
+}
+
+#pragma mark - UICollectionViewDelegate
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    
+    //计算当前页码
+    NSInteger pageNum = scrollView.contentOffset.x / screenW;
+    UIButton *btn = self.btnArray[pageNum];
+    //将按钮设为选中状态
+    [self btnClick:btn];
 }
 @end
