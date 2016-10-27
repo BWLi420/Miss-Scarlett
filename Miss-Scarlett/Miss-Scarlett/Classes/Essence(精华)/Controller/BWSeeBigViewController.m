9582e70bd12c4aa3a924f8e75625a3d9e2db75fc
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-27 20:13:35 +0800

    利用 scrollview 的特性进行大图缩放

--- Miss-Scarlett/Miss-Scarlett/Classes/Essence(精华)/Controller/BWSeeBigViewController.m
+++ Miss-Scarlett/Miss-Scarlett/Classes/Essence(精华)/Controller/BWSeeBigViewController.m
@@ -11,9 +11,9 @@
 
 #import <UIImageView+WebCache.h>
 
-@interface BWSeeBigViewController ()
+@interface BWSeeBigViewController () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
+@property (nonatomic, weak) UIImageView *imageView;
 @end
 
 @implementation BWSeeBigViewController
@@ -24,6 +24,7 @@
     //加载图片
     UIImageView *imageView = [[UIImageView alloc] init];
     [imageView sd_setImageWithURL:[NSURL URLWithString:self.item.image0]];
+    self.imageView = imageView;
     
     [self.scrollView addSubview:imageView];
     
@@ -32,9 +33,22 @@
     
     if (self.item.is_bigPicture) {
         self.scrollView.contentSize = CGSizeMake(screenW, height);
+        
+        //利用 scrollview 的特性进行缩放图片
+        self.scrollView.delegate = self;
+        if (self.item.height > height) {
+            //设置最大缩放比例
+            self.scrollView.maximumZoomScale = self.item.height / height;
+        }
     }else {
         imageView.center = CGPointMake(screenW * 0.5, screenH * 0.5);
     }
+}
+
+//返回需要缩放的控件
+//进行缩放的时候才会调用
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
 }
 
 - (IBAction)backClick:(UIButton *)sender {
